8da360afd0a401b33fcef7e5ae6ff48c0b2fb83f
Jason Pitts <juniorjr083@gmail.com>
2018-08-05 01:33:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  
+  @@cart = ["Cell Phone", "Beach ball", "taco"]
 
   def call(env)
     resp = Rack::Response.new
@@ -16,6 +18,27 @@ class Application
     else
       resp.write "Path Not Found"
     end
+    
+    if req.path.match(/cart/)
+       if @@cart.empty? 
+       resp.write "Your cart is empty"
+     else 
+        @@cart.each do |ele|
+          resp.write "#{ele}\n"
+          
+        end 
+        
+     end 
+   end
+   
+   if req.path.match(/add/)
+       ans = req.params["item"]
+        @@items.include?(ans) ? @@cart << ans : resp.write "Sorry, this is an error message that has been rendered beacuse you entered the wrong information  that has noting  to do  with the last tune tat has been entered into the string."
+        
+        
+     
+        
+   end 
 
     resp.finish
   end
