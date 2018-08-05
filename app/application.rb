77e3adba84293c07981ad1cddb0e5d8436f18bc0
Jason Pitts <juniorjr083@gmail.com>
2018-08-05 01:39:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-  
-  @@cart = ["Cell Phone", "Beach ball", "taco"]
-
+  @@cart = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -15,34 +13,25 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |ele|
+          resp.write "#{ele}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      ans = req.params["item"]
+      if @@items.include?(ans)
+        @@cart << ans
+        resp.write "added #{ans}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
-    
-    if req.path.match(/cart/)
-       if @@cart.empty? 
-       resp.write "Your cart is empty"
-     else 
-        @@cart.each do |ele|
-          resp.write "#{ele}\n"
-          
-        end 
-        
-     end 
-   end
-   
-   if req.path.match(/add/)
-       ans = req.params["item"]
-        if @@items.include?(ans) 
-          
-          @@cart << ans 
-        else 
-          resp.write "Sorry, this is an error message."
-        
-        
-     
-       end 
-   end 
 
     resp.finish
   end
